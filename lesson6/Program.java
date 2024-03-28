81615574870d53916fcb1d845bf0e7032ec1df99
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-28 16:49:21 +0300

    Modified class Program (main)

--- lesson6/Program.java
+++ lesson6/Program.java
@@ -11,8 +11,8 @@ public class Program {
      */
     public static void main(String[] args) {
         System.out.println("Введите заказ:");
-        Order order = new Order("", "", 0, 0);
-        order.inputFromConsole();
-        order.saveToJson();
+        Order order = new Order();
+        InputConsole.inputFromConsole(order);
+        SaverJson.saveToJson(order);
     }
 }
