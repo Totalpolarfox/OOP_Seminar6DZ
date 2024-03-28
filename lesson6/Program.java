ac8230db35017b66d6f01a68a5c40ceb884665bf
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-28 16:34:12 +0300

    Added class Program (main)

---
+++ lesson6/Program.java
@@ -0,0 +1,18 @@
+package lesson6.srp2;
+
+public class Program {
+
+    /**
+     * TODO: Переработать структуру приложения Order, приложение должно
+     * соответствовать
+     * принципу SRP.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Введите заказ:");
+        Order order = new Order("", "", 0, 0);
+        order.inputFromConsole();
+        order.saveToJson();
+    }
+}
